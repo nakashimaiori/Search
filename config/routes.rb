2b217f6e3e2ed10fdace44cee04d4338b1e5f272
vagrant <vagrant@localhost.localdomain>
2020-07-14 09:21:28 +0000

    Bootstrap導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  get root to: 'homes#top'
+  root 'books#index'
+  get 'top' => 'homes#top'
   get 'home/about' => 'homes#about'
+  resources :books, only: [:create, :index, :show, :edit, :destroy]
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20200714042906_create_books.rb
+++ db/migrate/20200714042906_create_books.rb
@@ -3,7 +3,7 @@ class CreateBooks < ActiveRecord::Migration[5.2]
     create_table :books do |t|
       t.text :title
       t.text :body
-      t.interger :user_id
+      t.integer :user_id
 
       t.timestamps
     end
